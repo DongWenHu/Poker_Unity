af48a8d5d49d94fcc560f5ee1440960c9926a193
arck-EXE <ankurgupta25.98@gmail.com>
2020-09-29 23:40:47 +0530

    Added : Login Page

---
+++ Royal Club Poker/Assets/Scripts/GameManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    public GameObject _registerLoginscreen;
+    public GameObject _loginscreen;
+
+    public void RegisterLogin()
+    {
+        _registerLoginscreen.gameObject.SetActive(true);
+        _loginscreen.gameObject.SetActive(false);
+    }
+
+    public void closebutton()
+    {
+        _registerLoginscreen.gameObject.SetActive(false);
+        _loginscreen.gameObject.SetActive(true);
+    }
+}
